4609a3a22ccb18e6377be634b6bb48dbc60c08ec
Erwhann <erwhann.deiss@ig2i.centralelille.fr>
2026-03-27 15:51:43 +0000

    2nd

--- Puissance2i/reseau/session.c
+++ Puissance2i/reseau/session.c
@@ -39,22 +39,29 @@ int accepter_client(int fd_ecoute, struct sockaddr_in *addrClt) {
 }
 
 int connecter_serveur(const char *adresse, int port) {
-    int fd;
-    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
+    char port_str[8];
+    snprintf(port_str, sizeof(port_str), "%d", port);
+
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if (getaddrinfo(adresse, port_str, &hints, &res) != 0) {
+        fprintf(stderr, "Erreur : resolution de '%s' impossible.\n", adresse);
         return -1;
     }
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    if (inet_aton(adresse, &addr.sin_addr) == 0) {
-        return -1;
-    }
-    if (connect(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+
+    int fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (fd == -1) { perror("socket"); freeaddrinfo(res); return -1; }
+
+    if (connect(fd, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("connect");
         close(fd);
+        freeaddrinfo(res);
         return -1;
     }
+    freeaddrinfo(res);
     return fd;
 }
 
